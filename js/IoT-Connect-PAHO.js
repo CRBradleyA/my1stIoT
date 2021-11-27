cd978fa1679bcebcde4442e7c92a01c4a451afd6
CRBradleyA <carlosr.bradley@gmail.com>
2021-11-27 14:04:50 -0400

    Update IoT-Connect-PAHO.js

--- js/IoT-Connect-PAHO.js
+++ js/IoT-Connect-PAHO.js
@@ -1,7 +1,7 @@
     // Cambios JS
 // ************ Declare and config MQTT
     var mqtt;
-    var clientId = "client-" + String(Math.floor(Math.random() * 1000000))
+    var clientId = "client-" + String(Math.floor(Math.random() * 1000000));
     var reconnectTimeout = 2000;
     var host="broker.emqx.io";
     var port=8083;
@@ -13,22 +13,22 @@
       message.destinationName = "esp8266/ledStatus_bradley";
       mqtt.send(message);
       onMessageSend("cliConnected"); // Send Msg for inicial status
-    }
+    };
 
     function onFailure(message){
       console.log('Connection Attemp to Host: ' + host + ' Failed.');
       setTimeout(MQTTconnect, reconnectTimeout);
-    }
+    };
 
     function onMessageArrived(msg){
       out_msg="Message received: " + msg.payloadString + " from: " + msg.destinationName + "<br>";
       console.log(out_msg);
-      actionReceived("estado", msg.payloadString)
-    }
+      actionReceived("estado", msg.payloadString);
+    };
 
     function actionReceived(id, Action){
-      var idElement = id
-      var txtElement = Action
+      var idElement = id;
+      var txtElement = Action;
       if (Action=="Connected"){
         idElement = "conectar"
         txtElement = Action + " to EMQX"
@@ -44,8 +44,8 @@
           document.getElementById("Luz1").setAttribute("stop-color", "#aa1010");
         }
         else {
-          var Obj_Json = JSON.parse(Action)
-          txtElement = "Led 1: " + Obj_Json["led1"]
+          var Obj_Json = JSON.parse(Action);
+          txtElement = "Led 1: " + Obj_Json["led1"];
           if (Obj_Json["led1"]=="off"){
             document.getElementById("Led1").setAttribute("stop-color", "#000000");
             document.getElementById("Luz1").setAttribute("stop-color", "#820101");
@@ -53,19 +53,19 @@
           else if (Obj_Json["led1"]){
             document.getElementById("Led1").setAttribute("stop-color", "#ff0000");
             document.getElementById("Luz1").setAttribute("stop-color", "#aa1010");
-          }
+          };
 
           g2.refresh(Obj_Json["temp1"]);
           g3.refresh(Obj_Json["hum1"]);
-        }
+        };
         document.getElementById(idElement).innerHTML = txtElement;
-      }
+      };
 //      var elem = document.getElementById(idElement);
-//	  console.log("Action Led1: " + Action["led1"])
-          console.log(idElement)
-          console.log(Action)
+//	  console.log("Action Led1: " + Action["led1"]);
+          console.log(idElement);
+          console.log(Action);
   //    elem.innerHTML = txtElement;
-    }
+    };
 
     function onMessageSend(msg){
       message = new Paho.MQTT.Message(msg);
@@ -74,7 +74,7 @@
       send_msg=send_msg + " to Topic: " + message.destinationName + "<br>";
       mqtt.send(message);
       console.log(send_msg);
-    }
+    };
 
     function MQTTconnect(){
       // console.log("Connecting to: " + host + ":" + port);
@@ -87,38 +87,38 @@
       };
       mqtt.onMessageArrived = onMessageArrived;
       mqtt.connect(options);
-    }
+    };
 
 // ************ Declare and config Gauges
-	var g2, g3; 
+    var g2, g3; 
 
-	window.onload = function(){
-	  g2 = new JustGage({
-	    id: "g2",
-	<!--          value: getRandomInt(0, 100),--!>
-	    value: 0,
-	    min: 0,
-	    max: 60,
-	    title: "Temperature",
-	    label: "ยบC"
-	  });
+    window.onload = function(){
+      g2 = new JustGage({
+        id: "g2",
+        <!-- value: getRandomInt(0, 100),--!>
+        value: 0,
+        min: 0,
+        max: 60,
+        title: "Temperature",
+        label: "ยบC"
+      });
 
-	  g3 = new JustGage({
-	    id: "g3",
-	    value: 0,
-	    min: 0,
-	    max: 100,
-	    title: "Humedad",
-	    label: "%H"
-	  });
+      g3 = new JustGage({
+        id: "g3",
+        value: 0,
+        min: 0,
+        max: 100,
+        title: "Humedad",
+        label: "%H"
+      });
 
 	<!-- setInterval(function() { -->
-	  <!-- g2.refresh(getRandomInt(50, 100)); -->
-	  <!-- g3.refresh(getRandomInt(0, 50)); -->
+	<!-- g2.refresh(getRandomInt(50, 100)); -->
+	<!-- g3.refresh(getRandomInt(0, 50)); -->
 	<!-- }, 2500); -->
-	};
+    };
 
 //    function changeColor(newColor, Action) {
 //       var elem = document.getElementById('buttonOn');
 //       elem.style.color = newColor;
-//    }
+//    };
